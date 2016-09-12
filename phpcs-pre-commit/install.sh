eda9f32863e089fa9de936a6cda1458b990ba98d
Eduardo Nascimento <eduardo@eduardonascimento.com.br>
2016-09-12 13:34:06 -0300

    Fix path

--- phpcs-pre-commit/install.sh
+++ phpcs-pre-commit/install.sh
@@ -1,2 +1,2 @@
-cp vendor/s0enke/git-hooks/phpcs-pre-commit/pre-commit .git/hooks/
-cp vendor/s0enke/git-hooks/phpcs-pre-commit/config .git/hooks/
+cp vendor/pagarme/git-hooks/phpcs-pre-commit/pre-commit .git/hooks/
+cp vendor/pagarme/git-hooks/phpcs-pre-commit/config .git/hooks/
